45e93e72cd7b2d37b4bb30066acd980556172119
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-09-24 21:39:43 +0000

    Implement chirp creation functionality with validation and success message

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ChirpController::class,'index'] );
+Route::post('/chirps',[ChirpController::class,'store'] );
 
-Route::resource('/chirp', ChirpController::class);
+
 
