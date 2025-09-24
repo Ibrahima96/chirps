bb2483a441a7084091f5a4e8d4047a06fdf9a752
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-09-24 14:59:07 +0000

    Add initial application structure with routes, tests, and configuration
    - Created console command for inspiring quotes in routes/console.php
    - Defined web routes and resource controller for chirps in routes/web.php
    - Added .gitignore files for storage directories to exclude unnecessary files
    - Implemented basic feature and unit tests in tests/Feature and tests/Unit
    - Configured Vite for asset management with Laravel and Tailwind CSS

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\ChirpController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',[ChirpController::class,'index'] );
+
+Route::resource('/chirp', ChirpController::class);
+
