8c71078ebe8f19e0466f33196221cdcff48a3e8a
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-09-26 15:39:18 +0000

    Add user registration functionality with validation and views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,12 @@ Route::get('/', [ChirpController::class, 'index']);
 
 Route::resource('chirps', ChirpController::class)
     ->only(['store', 'edit', 'update', 'destroy']);
+
+
+// Registration routes
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', Register::class)
+    ->middleware('guest');
